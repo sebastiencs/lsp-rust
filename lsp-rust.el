186af2fdbd63dea1739596384d99969dafbd4479
fmdkdd <fmdkdd@gmail.com>
2017-10-19 14:58:23 +0200

    Use `lsp-status` to signal the RLS is building
    This changes the mode-line to notify users that the RLS has started working.

--- lsp-rust.el
+++ lsp-rust.el
@@ -63,11 +63,11 @@
     ("rustDocument/diagnosticsEnd" .
      (lambda (w _p)
        (when (< (cl-decf (gethash w lsp-rust--diag-counters 0)) 0)
-	 (message "RLS: done"))))
+	 (setq lsp-status nil))))
     ("rustDocument/beginBuild" .
      (lambda (w _p)
        (cl-incf (gethash w lsp-rust--diag-counters 0))
-       (message "RLS: working")))))
+       (setq lsp-status "(building)")))))
 
 (defun lsp-rust--render-string (str)
   (with-temp-buffer
